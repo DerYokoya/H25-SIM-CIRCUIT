9eb07ed6fec5ccbb2a0cc86ca328affc90212f20
DerYokoya <doryvl224@gmail.com>
2025-02-26 16:59:35 -0500

    Modifications
    Etirement modifié, prefab Fil, creerObjet fil

--- Assets/Scripts/ScriptFils.cs
+++ Assets/Scripts/ScriptFils.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Drag3DRectangle : MonoBehaviour
+public class EtirementRectangle : MonoBehaviour
 {
     private Vector3 decalage;  // Distance souris-rectangle
     private bool enEtirement = false;
     private Vector3 pointDepart;  // Point de départ de l'étirement
-    private Vector3 tailleInitiale;  // Taille initiale du rectangle
+    private Vector3 tailleInitiale;
+    private Vector3 positionInitiale;
 
     void Update()
     {
@@ -23,6 +24,7 @@ public class Drag3DRectangle : MonoBehaviour
                     decalage = gameObject.transform.position - laserTouche.point;
                     pointDepart = laserTouche.point;
                     tailleInitiale = transform.localScale;
+                    positionInitiale = transform.position;
                     enEtirement = true;
                 }
             }
@@ -38,12 +40,38 @@ public class Drag3DRectangle : MonoBehaviour
                 // Calculer le déplacement de la souris par rapport au point de départ
                 Vector3 deplacement = laserTouche.point - pointDepart;
 
-                // Ajuster la taille du rectangle en fonction du déplacement
-                transform.localScale = new Vector3(
-                    tailleInitiale.x + deplacement.x,
-                    tailleInitiale.y,
-                    tailleInitiale.z
-                );
+                // Déterminer la direction du déplacement (gauche ou droite)
+                float nouvelleLargeur = tailleInitiale.x + Mathf.Abs(deplacement.x); // Taille toujours positive
+                float direction = Mathf.Sign(deplacement.x); // -1 pour gauche, 1 pour droite
+
+                // Limiter la largeur pour éviter des valeurs négatives ou trop petites
+                if (nouvelleLargeur > 0)
+                {
+                    // Ajuster la taille du rectangle
+                    transform.localScale = new Vector3(
+                        nouvelleLargeur,
+                        tailleInitiale.y,
+                        tailleInitiale.z
+                    );
+
+                    // Ajuster la position en fonction de la direction
+                    if (direction > 0) // Tirer vers la droite
+                    {
+                        transform.position = new Vector3(
+                            positionInitiale.x + deplacement.x / 2,
+                            positionInitiale.y,
+                            positionInitiale.z
+                        );
+                    }
+                    else // Tirer vers la gauche
+                    {
+                        transform.position = new Vector3(
+                            positionInitiale.x + deplacement.x / 2,
+                            positionInitiale.y,
+                            positionInitiale.z
+                        );
+                    }
+                }
             }
         }
 
