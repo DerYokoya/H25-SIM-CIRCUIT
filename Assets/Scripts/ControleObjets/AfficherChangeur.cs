1e887af6daa8ad327bda643318f7b5b931874161
DerYokoya <doryvl224@gmail.com>
2025-04-02 16:52:37 -0400

    BlocInfos et resistance
    Le BlocInfos se déplace avec l'objet
    La resistance a une nouvelle texture

--- Assets/Scripts/ControleObjets/AfficherChangeur.cs
+++ Assets/Scripts/ControleObjets/AfficherChangeur.cs
@@ -35,6 +35,9 @@ public class AfficherChangeur : MonoBehaviour
             {
                 texte.text = composanteActuelle.TexteValeur();
             }
+
+            //Suivre la composante du circuit
+            changeurActuel.transform.position = transform.position + new Vector3(0, 1.25f, 0);
         }
 
         // Gestion des clics sur les boutons
