2436c8e62c8ae1394ab9c7ea34644076ee12504a
DerYokoya <133297414+DerYokoya@users.noreply.github.com>
2025-04-14 10:36:30 -0400

    Ampoule et BlocInfos
    Pour que ce soit clair, le blocInfos a des boutons verts pour ajouter et rouge pour enlever à la place. J'ai ajouté la puissance et la tension au scriptAmpoule, ce qui va nous permettre de calculer la puissance, ce qui va nous permettre de calculer la luminosité

--- Assets/Scripts/ComposantesDuCircuit/ScriptAmpoules.cs
+++ Assets/Scripts/ComposantesDuCircuit/ScriptAmpoules.cs
@@ -1,15 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class Ampoule : Resistance
 
 
 {
+    public double Puissance { get; set; }
+    public double Tension { get; set; }
     public double Luminosite { get; set; }
 
     public void ChangementLuminosite()
     {
+    }
+
+    public void CalculPuissance()
+    {
+        Puissance = Math.Pow(Tension, 2) / ValeurResistance; // Calcul de la puissance, qui va déterminer la luminosité
     }
 
 }
